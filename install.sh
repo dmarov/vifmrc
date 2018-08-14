907abc1de96fa29aa81af21d635dd8e8caec4d8a
Dmitry Marov <d.marov94@gmail.com>
2018-08-14 17:21:01 +0300

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,19 @@
 [ -d ~/.vifm ] && rm -r ~/.vifm;
 cd $(dirname $0);
 ln -s "$(pwd -P)" ~/.vifm
+
+[ $(uname) = 'FreeBSD' ] && \
+    sudo pkg install -y \
+        fusefs-sshfs \
+        fusefs-curlftpfs \
+        vim \
+        neovim \
+        xfce4-terminal \
+        libreoffice \
+        smplayer \
+        audacious \
+        atril \
+        geeqie \
+        umbrello \
+        xmlcopyeditor
+
